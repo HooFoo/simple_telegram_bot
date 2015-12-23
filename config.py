91beb6f8bf8de57c31f280bc72c6572a2e255d07
Unknown Name <hoofoo>
2015-12-23 11:57:09 +0000

    config.py deleted online with Bitbucket

--- config.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-token = '112646377:AAF5jGjRWoxYWkGTPb7aF0AZmXdoJ7bl6R8'
-name = 'rakamakafo_bot'
-owner = 'HooFoo'
-BING_API_KEY= '8TD+J9slKVfmFtSAvelvy3BAyDlezRlofIJTZyoL1J4='
